2dc6e1610b12c35415bbfe214a1e1ab29998888d
ehfdkdhswlgh <ehfdkdhswlgh@naver.com>
2023-04-11 22:24:36 +0900

    dd

--- .vscode/class3/1764.cpp
+++ .vscode/class3/1764.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <set>
+#include <vector>
+#include <algorithm>
 //set : 집합과 같고 삽입과 동시에 자동으로 정렬됨
 //빠른 검색으로 존재 여부를 신속하게 알고 싶을때
 
@@ -12,6 +14,7 @@ int main()
 	cin.tie(NULL);
     
 	set<string> s;
+    vector<string> v;
 
     int n, m;
     cin >> n >> m;
@@ -31,15 +34,17 @@ int main()
         
         if(s.find(str) != s.end()) //값이 존재한다면
         {
-            s.erase(str);
+            v.push_back(str);
         }
     }
 
-    cout << s.size() << '\n';
 
-    for(auto iter = s.begin(); iter != s.end(); iter++)
+    sort(v.begin(), v.end());
+    cout << v.size() << '\n';
+
+    for(auto i : v)
     {
-        cout << *iter << '\n';
+        cout << i << '\n';
     }
 
 }
